e2c976ef0c199da68caf8d9424210e9558c6f095
Robert Picard <mail@robert.io>
2015-04-20 16:38:06 -0700

    fiddle with cli stuff

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+    "github.com/jawher/mow.cli"
+    "os"
 )
 
 // we want to get n bytes from a random keystream for the stream ciphers
@@ -9,16 +11,23 @@ type RandomKeyStreamer interface {
 }
 
 func main() {
-    sc := NewStreamCounter(256)
 
-    streamer := NewRc4Streamer()
+    app := cli.App("stream-bias", "chart potential biases in stream cipher keystreams")
 
-    for i := 0; i < 1000000; i++ {
-        sc.AddBytes(streamer.RandomKeyStream(256))
+    app.Command("generate" func() {
+        sc := NewStreamCounter(256)
+
+        streamer := NewRc4Streamer()
+
+        for i := 0; i < 1000000; i++ {
+            sc.AddBytes(streamer.RandomKeyStream(256))
+        }
+
+        page := NewChartPage(sc)
+
+        page.PrintHtml()
     }
 
-    page := NewChartPage(sc)
-
-    page.PrintHtml()
+    app.Run(os.Args)
 }
 
