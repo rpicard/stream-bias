bb64c2acb6815de95f6183eb5f11068b601f8c56
Robert Picard <mail@robert.io>
2015-04-20 17:56:23 -0700

    add some options to the cli

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
     "os"
     "log"
     "errors"
+    "strconv"
 )
 
 // we want to get n bytes from a random keystream for the stream ciphers
@@ -18,8 +19,10 @@ func main() {
 
     format := app.StringOpt("f format", "html", "output format (html or json)")
     cipher := app.StringOpt("c cipher", "", "which cipher are we testing?")
+    samples := app.StringOpt("s samples", "1000", "how many samples should we take?")
+    length := app.StringOpt("l length", "256", "how many positions in the keystream do we care about?")
 
-    app.Spec = "--cipher [--format]"
+    app.Spec = "--cipher [--format] [--samples] [--length]"
 
     app.Action = func() {
         sc := NewStreamCounter(256)
@@ -32,8 +35,19 @@ func main() {
             default: log.Fatal(errors.New("I do not know that cipher."))
         }
 
-        for i := 0; i < 1000; i++ {
-            sc.AddBytes(streamer.RandomKeyStream(256))
+        // parse the number of samples and stream length into int64s
+        numSamples, err := strconv.ParseInt(*samples, 10, 64)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        streamLength, err := strconv.Atoi(*length)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        for i := int64(0); i < numSamples; i++ {
+            sc.AddBytes(streamer.RandomKeyStream(streamLength))
         }
 
         // print out the data as either html charts or json
